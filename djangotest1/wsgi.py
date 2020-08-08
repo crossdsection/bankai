72921c602bf764182d2b525808bf19f713f67799
Piyush Kumar <chirkut.kr@gmail.com>
2020-08-08 23:54:26 +0530

    restarting python base

--- djangotest1/wsgi.py
+++ djangotest1/wsgi.py
@@ -1,16 +1,16 @@
 """
-WSGI config for bankai project.
+WSGI config for djangotest1 project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 """
 
 import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bankai.settings")
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangotest1.settings')
 
 application = get_wsgi_application()
